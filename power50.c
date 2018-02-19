42aaf03ec261e1ed2c41c9fd139c0b51290f6bd5
manisu <35994839+manisu@users.noreply.github.com>
2018-02-19 16:01:18 +0530

    Create power50.c

---
+++ power50.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+int n;
+printf("\n enter the number");
+scanf("%d",&n);
+if(n%2==0)
+printf("\n the number is a power of 2");
+else
+printf("\n the number is not the power of 2");
+getch();
+}
