e99040472d0f5aff0200b2797e07304ffab119dd
manisu <35994839+manisu@users.noreply.github.com>
2018-02-21 15:38:45 +0530

    Create nearesteven54.c

---
+++ nearesteven54.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+int n,l;
+printf("\n enter the number");
+scanf("%d",&n);
+if(n%2==0)
+{
+printf("\n %d",n);
+}
+else
+{
+l=n-1;
+printf("\n %d",l);
+}
+getch();
+}
